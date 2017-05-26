c6fa18c8be9d9fefaace854ae38fd64f56f998a2
Roy Macdonald <macdonald.roy@gmail.com>
2017-05-26 19:16:56 -0400

    Agregado script instalacion addons

---
+++ install_addons.sh
@@ -0,0 +1,9 @@
+ofPath="/c/of/"
+
+cd "$ofPath"
+cd addons
+git clone https://github.com/elliotwoods/ofxKinectForWindows2.git
+git clone https://github.com/arturoc/ofxEasing.git
+git clone https://github.com/kylemcdonald/ofxCv.git
+git clone -b astana https://github.com/roymacdonald/ofxSoundObjects.git
+
