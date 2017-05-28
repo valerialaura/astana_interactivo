41468cd558793e0ac70e78a05bc2b8d58cf3c9fa
Roy Macdonald <macdonald.roy@gmail.com>
2017-05-28 00:59:36 -0400

    actualizado script install_addons.sh

--- install_addons.sh
+++ install_addons.sh
@@ -6,4 +6,6 @@ git clone https://github.com/elliotwoods/ofxKinectForWindows2.git
 git clone https://github.com/arturoc/ofxEasing.git
 git clone https://github.com/kylemcdonald/ofxCv.git
 git clone -b astana https://github.com/roymacdonald/ofxSoundObjects.git
+git clone https://github.com/roymacdonald/ofxTween2.git
+
 
