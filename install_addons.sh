fd613e34e9742ce9b9a974b9d15fb98d4fa44b5d
Roy Macdonald <macdonald.roy@gmail.com>
2017-05-31 17:57:24 -0400

    Update script install_addons.sh

--- install_addons.sh
+++ install_addons.sh
@@ -3,7 +3,7 @@ ofPath="/c/of/"
 cd "$ofPath"
 cd addons
 git clone -b astana https://github.com/roymacdonald/ofxKinectForWindows2.git
-git clone https://github.com/arturoc/ofxEasing.git
+git clone -b fix-useInWindows https://github.com/roymacdonald/ofxEasing.git
 git clone -b astana https://github.com/roymacdonald/ofxCv.git
 git clone -b astana https://github.com/roymacdonald/ofxSoundObjects.git
 git clone https://github.com/roymacdonald/ofxTween2.git
